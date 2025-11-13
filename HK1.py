70a2488d3a85339b8daae39d27a60ed8108fa1d7
25021516-cell <25021516@vnu.edu.vn>
2025-11-13 15:33:31 +0700

    d

--- HK1.py
+++ HK1.py
@@ -16,6 +16,19 @@ print('\n')
 #W2A19
 print('W2A19 \n')
 
-print('Monday \nTuesday \nWednesday \nThursday \nFriday \nSaturday \nSunday \n')
+print('Monday \nTuesday \nWednesday \nThursday \nFriday \nSaturday \nSunday')
 print('\n')
 
+#W2A20
+print('W2A1920 \n')
+
+print('January \nFebruary \nMarch \nApril \nMay \nJune \nJuly \nAugust \nSeptember \nOctober \nNovember \nDecember')
+print('\n')
+
+#W2A21
+print('W2A21 \n')
+
+for i in range(10):
+    print('Hello World')
+
+print('\n')
