04224ae15b4cea4583dc291e1a6ff3f3439cf86e
25021516-cell <25021516@vnu.edu.vn>
2025-11-13 15:31:20 +0700

    j

--- HK1.py
+++ HK1.py
@@ -11,3 +11,11 @@ print(' #  # #   #   #')
 print(' #  ##    #   #')
 
 print('### ##   ### ### \n #  # #   #   # \n #  #  #  #   # \n #  # #   #   # \n #  ##    #   #')
+print('\n')
+
+#W2A19
+print('W2A19 \n')
+
+print('Monday \nTuesday \nWednesday \nThursday \nFriday \nSaturday \nSunday \n')
+print('\n')
+
